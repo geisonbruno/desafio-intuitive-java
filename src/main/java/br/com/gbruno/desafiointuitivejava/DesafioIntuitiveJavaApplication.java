a1f6e742c33a11c12f265f1cc784042b7b11e16d
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-03-25 15:22:03 -0300

    initial commit

---
+++ src/main/java/br/com/gbruno/desafiointuitivejava/DesafioIntuitiveJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gbruno.desafiointuitivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIntuitiveJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioIntuitiveJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gbruno/desafiointuitivejava/DesafioIntuitiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gbruno.desafiointuitivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioIntuitiveJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
